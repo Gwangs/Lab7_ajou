ab33f801697c089f77072b29f352aed3d877fa55
Gwans <hanson119@ajou.ac.kr>
2017-05-01 10:59:18 +0900

    create product class

---
+++ Lab7/src/kr_ac_ajou/Product.java
@@ -0,0 +1,20 @@
+package kr_ac_ajou;
+
+public abstract class Product {
+	private double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		if (price < 0) {
+			System.out.println("error");
+			System.exit(0);
+		} else
+			this.price = price;
+	}
+
+	public abstract String getName();
+
+}
