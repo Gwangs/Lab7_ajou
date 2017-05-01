086743a13e66e2feaf92f42ed7825c03c50b7285
Park <pjcjc1995@naver.com>
2017-05-01 11:22:00 +0900

    create Networked interface + laptop

---
+++ Lab7/src/kr_ac_ajou/Laptop.java
@@ -0,0 +1,42 @@
+package kr_ac_ajou;
+
+public class Laptop extends Product implements Networked{
+	private double totalCapacity;
+	private double usedCapacity;
+	
+	public double getFreeCapacity(){
+		
+	}
+	
+	public void format(){
+		
+	}
+	
+	public boolean isConnected(){
+		
+	}
+	
+	public double maxSpeed(){
+		
+	}
+	
+	public double getTotalCapacity(){
+		
+	}
+	
+	public void setTotalCapacity(double ){
+		
+	}
+	
+	public double getUsedCapacity(){
+		
+	}
+	
+	public void setUsedCapacity(double ){
+		
+	}
+	
+	public String getName(){
+		
+	}
+}
