3ae69d93b5256bffdedff8eac03952d6110114a0
Park <pjcjc1995@naver.com>
2017-05-01 11:50:28 +0900

    수정중

--- Lab7/src/kr_ac_ajou/Laptop.java
+++ Lab7/src/kr_ac_ajou/Laptop.java
@@ -5,7 +5,7 @@ public class Laptop extends Product implements Networked{
 	private double usedCapacity;
 	
 	public double getFreeCapacity(){
-		
+		return;
 	}
 	
 	public void format(){
@@ -21,19 +21,19 @@ public class Laptop extends Product implements Networked{
 	}
 	
 	public double getTotalCapacity(){
-		
+		return totalCapacity;
 	}
 	
-	public void setTotalCapacity(double ){
-		
+	public void setTotalCapacity(double totalCapacity){
+		this.totalCapacity = totalCapacity;
 	}
 	
 	public double getUsedCapacity(){
-		
+		return usedCapacity;
 	}
 	
-	public void setUsedCapacity(double ){
-		
+	public void setUsedCapacity(double usedCapacity){
+	if()	this.usedCapacity = usedCapacity;
 	}
 	
 	public String getName(){
