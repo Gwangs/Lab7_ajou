690ffae995bc3b87177d9e8b1f7efd4e70f83262
Gwangs <hanson119@ajou.ac.kr>
2017-05-04 22:06:07 +0900

    이거 아무것도 아니에요

--- Lab7/src/kr_ac_ajou/Laptop.java
+++ Lab7/src/kr_ac_ajou/Laptop.java
@@ -1,28 +1,49 @@
 package kr_ac_ajou;
 
-public class Laptop extends Product implements Networked{
+public class Laptop extends Product implements DataStorage, Networked{
 	private double totalCapacity;
 	private double usedCapacity;
 	
 	
 	public double getFreeCapacity(){
-		return getFreeCapacity();
+		double freecapacity = totalCapacity - usedCapacity;
+		System.out.print("Free Capacity is ");
+		
+		return freecapacity;
 	}
 	
 	public void format(){
-		format();
+		System.out.println("It's 15.6 inches wide.");
 	}
 	
 	@Override
 	public boolean isConnected(){
-		return isConnected();
+		boolean connected = true;
+		System.out.println("Internet Connected condition: " +connected);
+		return connected;
 	}
 	
 	@Override
 	public double maxSpeed(){
-
-		return maxSpeed();
-
+		System.out.println();
+		System.out.println("Now Checking internet conditon");
+		
+		double max = 67.30;
+		double ping = 0.007;
+		System.out.println("Internet Speed: " +max+ " Mbps");
+		System.out.println("Ping: " +ping+ " Seconds");
+		
+			if(max > 30) {
+			System.out.println("Internet Speed Condition : OK");
+		}
+		if(max < 70) {
+			System.out.println("Speed is fine, but not fastest.");
+		}
+		if(ping < 0.05) {
+			System.out.println("There is no delay in internet.");
+		}
+		
+		return max;
 	}
 	
 	public double getTotalCapacity(){
@@ -30,7 +51,12 @@ public class Laptop extends Product implements Networked{
 	}
 	
 	public void setTotalCapacity(double totalCapacity){
-		this.totalCapacity = totalCapacity;
+		if(totalCapacity > 0) {
+			this.totalCapacity = totalCapacity;
+		}
+		else {
+			System.out.println("Invalid!!");
+		}
 	}
 	
 	public double getUsedCapacity(){
@@ -38,11 +64,17 @@ public class Laptop extends Product implements Networked{
 	}
 	
 	public void setUsedCapacity(double usedCapacity){
-		this.usedCapacity = usedCapacity;
+		if(usedCapacity > 0) {
+			this.usedCapacity = usedCapacity;
+		}
+		else {
+			System.out.println("Invalid!!");
+		}
 	}
 	
 	@Override
 	public String getName(){
-		return "Laptop name: "+getName();
+		System.out.println("Laptop name: Samsung vision wide");
+		return "laptop name: ";
 	}
 }
--- Lab7/src/kr_ac_ajou/ProductTest.java
+++ Lab7/src/kr_ac_ajou/ProductTest.java
@@ -1,44 +1,64 @@
 package kr_ac_ajou;
 
-/*
-import test.Cooker;
-import test.Product;
-import test.Toaster;
-*/
+import java.util.Scanner;
 
 public class ProductTest {
 
 	public static void main(String[] args){
-		//Laptop lap = new Laptop();
+		Scanner scan = new Scanner(System.in);
 		
-		//예시 이렇게 하면 될것 같아요, setter에 가디언 붙여주세요(조건) 
-		//목요일 10시까지 마무리하는 걸로 하죠
-		//한글 주석은 지워도 돼요. import도 굳이 안해줘도 돼요. 같은 패키지에 있으니까....
+		ProductTest pt = new ProductTest();
+		System.out.print("Type the Channel number. ");
+		int channel = scan.nextInt();
+		SmartTV smart = new SmartTV();
+		smart.getName();
+		smart.setchannel(channel);
+		smart.getchannel();
+		smart.isConnected();
+		smart.maxSpeed();
+		
+		
+		Laptop laptop = new Laptop();
+		double usedcap = 52.8;
+		double totalcap = 100.3;
+		laptop.getName();
+			
+		laptop.setUsedCapacity(usedcap);
+		laptop.setTotalCapacity(totalcap);
+		laptop.format();
+		laptop.isConnected();
+		laptop.maxSpeed();
+		
 
 		Toaster t = new Toaster(35.6);
 				
-		testProduct(t);
-		testCooker(t);
-
-		
+		pt.testProduct(t);
+		pt.testCooker(t);
+		pt.testDataStorage(laptop);
+		scan.close();
 	}
-	//예시
-	public static void testProduct(Product p){
+
+	public void testProduct(Product p){
 		System.out.println("price : "+p.getPrice());
 		System.out.println("Name : "+p.getName());
 	}
 	
-/*
-	public void testDadaStorage(DataStorage){
-		
+	
+	public void testDataStorage(DataStorage d) {
+		System.out.println("Free Capacity : " + d.getFreeCapacity());
+		d.format();
 	}
 
-	public void testNetworked(Networked){
-		
-	}
-*/
 
-	public static void testCooker(Cooker c){
+	public void testNetworked(Networked n) {
+		if(n.isConnected() == false) {
+			System.out.println("Connection state : Not connnected");
+		} else {System.out.println("Connection state : Connnected");}
+		
+		System.out.println("Max Speed : " + n.maxSpeed() + "\n");
+	}
+
+	public void testCooker(Cooker c){
 		System.out.println(c.prepareFood());
 	}
 	
